4665afc3dbcb9dfeb742148fead3534ea5c5be39
Steve Walker <captnwalker@gmail.com>
2018-02-21 15:57:48 -0500

    wired up edit and delete record controls

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-      <div> 
-      
-        <h3>{description}</h3>
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+
+    <div> 
+
+        <Link to={`/edit/${id}`}>
+
+            <h3>{description}</h3>
+
+        </Link>
 
         <p>{amount} - {createdAt}</p>
-
-        <button onClick={() => {
-            dispatch(removeExpense({ id }));
-        }}>Delete Invoice</button>
 
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
