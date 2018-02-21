64d545f4a980bfdd7e233840bea5bbbf875273ae
Steve Walker <captnwalker@gmail.com>
2018-02-20 23:39:01 -0500

    basic sorting functioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => (
     <header>
-        <h1>Expense Manager</h1>
+        <h1>Restaurant Expense Tracker</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Enter Invoice</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
