44e9840ce86543e7c84b1f49dc8cd7823c4e6a31
Steve Walker <captnwalker@gmail.com>
2018-02-22 17:19:50 -0500

    resolving git conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-<<<<<<< HEAD
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
@@ -48,52 +47,3 @@ module.exports = (env) => {
     }
   };
 };
-=======
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-module.exports = (env) => {
-const isProduction = env === 'production';
-const CSSExtract = new ExtractTextPlugin('styles.css');
-
-  return {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: CSSExtract.extract({
-      use: [
-       {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true
-          }
-        },
-        {
-          loader: 'sass-loader',
-           options: {
-            sourceMap: true
-          }
-         }
-        ]
-      })
-    }]
-  },
-  plugins: [
-    CSSExtract
-  ], 
-  devtool: isProduction ?  'source-map' : 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-};
-};
->>>>>>> d094a506c47e93d589499a5250e86e7881f5f7a8
