0696cdb712d0df5de6f4306d0d2dbb0e9846c33b
Steve Walker <captnwalker@gmail.com>
2018-02-24 16:44:05 -0500

    correct dotenv refs in testsjs

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,13 +1,17 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({
-    adapter: new Adapater()
-});
+import DotEnv from 'dotenv';
+
+DotEnv.config({ path: '.env.test' });
+
+
+
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// Enzyme.configure({
+//     adapter: new Adapater()
+// });
 
 
 //require('dotenv').config({ path: '.env.test' });
 
-// import DotEnv from 'dotenv';
-
-// DotEnv.config({ path: '.env.test' });
