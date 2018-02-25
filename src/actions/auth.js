257a6f513ebc4027d3993161e73ab93d05c9716c
Steve Walker <captnwalker@gmail.com>
2018-02-25 15:20:25 -0500

    securing authentication

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,23 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+// Capture login id
+export const login = (uid) => ({
+    type: 'LOGIN',
+    uid
+});
+
+// Initiate Google login with a pop-up window
 export const startLogin = () => {
     return () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
     };
 };
 
+export const logout = () => ({
+    type: 'LOGOUT'
+});
+
+// Log user out of Firebase
 export const startLogout = () => {
     return () => {
         return firebase.auth().signOut();
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,22 @@
 import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
 
 // Building our store with redux and redux-thunk middleware
 // Redux is a predictable state container for JavaScript apps. Esentially, Flux for React
 // Redux-Thunk is a function that wraps an expression to delay its evaluation
 
-const composeEnhancers = window.__REDUX_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
     const store = createStore(
         combineReducers({
             expenses: expensesReducer,
-            filters: filtersReducer
+            filters: filtersReducer,
+            auth: authReducer
         }),
 
         composeEnhancers(applyMiddleware(thunk))       
